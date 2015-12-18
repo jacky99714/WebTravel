dfb80ac18d3fc4c81ce87c22ed2d7e05093ecf23
chenchen <Student@III-ITC>
2015-12-18 16:47:45 +0800

    modify build file add json jar add GetQuestionServlet modify dao

---
+++ WebTravel/src/model/dao/QDAO.java
@@ -0,0 +1,24 @@
+package model.dao;
+
+import java.util.List;
+
+import model.QBean;
+
+public interface QDAO {
+
+	List<QBean> select();
+
+	QBean select(int qId);
+
+	QBean select(String qName);
+
+	boolean delete(int qId);
+
+	QBean insert(QBean qBean);
+
+	QBean update(QBean qBean);
+
+
+	int getCount();
+
+}
