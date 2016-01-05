80b991f5d02d5cb92fefe5b21e18d0882e95db94
star <wmstar.chen@gmail.com>
2016-01-05 15:01:07 +0800

    新增service

---
+++ WebTravel/src/model/service/AddFavoriteService.java
@@ -0,0 +1,21 @@
+package model.service;
+
+import java.util.List;
+
+import model.bean.CollectBean;
+import model.dao.CollectDAO;
+import model.dao.jndi.CollectDAOjndi;
+
+public class AddFavoriteService {
+	CollectDAO collectdao = new CollectDAOjndi();
+	//新增收藏
+	public List<CollectBean> addFavorite(CollectBean cBean) {
+		collectdao.insert(cBean);
+		
+		
+		return null;
+	}
+	
+	
+	
+}
