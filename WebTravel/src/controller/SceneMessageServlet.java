96e6c7686107b737729915ae86dfd5f3d16c1bc9
star <wmstar.chen@gmail.com>
2016-01-08 21:36:22 +0800

    1/8

---
+++ WebTravel/src/controller/SceneMessageServlet.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.bean.MemberBean;
+import model.bean.SceneMessageBean;
+
+/**
+ * Servlet implementation class SceneMessage
+ */
+@WebServlet("/SceneMessage")
+public class SceneMessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+		
+		//接收
+		String smes = request.getParameter("message");
+		
+		String sid = request.getParameter("sceneId");
+		
+		HttpSession session = request.getSession();
+		MemberBean mb = (MemberBean)session.getAttribute("loginOk");
+		int mid = mb.getMemberId();
+		//驗證
+		
+		if(smes != null ){
+			SceneMessageBean smb = new SceneMessageBean();
+			smb.setMemberId(mid);
+			smb.setSceneId(Integer.valueOf(sid));
+			smb.setMessageContent(smes);
+			
+		}
+		
+	
+		
+		
+		//轉換
+		
+		//model
+		
+		
+		//view
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(request, response);
+	}
+
+}
