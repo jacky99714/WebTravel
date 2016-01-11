cb52560cc340a99a20878715ec0d883de6200787
chenchen <Student@III-ITC>
2016-01-11 11:20:07 +0800

    add hibernate.cfg.xml

---
+++ WebTravel/src/model/hibernate/QDAOHibernate.java
@@ -0,0 +1,63 @@
+package model.hibernate;
+
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import model.bean.QBean;
+import model.dao.QDAO;
+
+
+public class QDAOHibernate implements QDAO{
+	
+	private Session session;
+	public QDAOHibernate(Session session) {
+		super();
+		this.session = session;
+	}
+	public Session getSession() {
+		return session;
+	}
+
+	@Override
+	public List<QBean> select() {
+		
+		return null;
+	}
+
+	@Override
+	public QBean select(String qName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public QBean select(int qId){
+		return (QBean) getSession().get(QBean.class, qId);	
+	}	
+
+	@Override
+	public boolean delete(int qId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public QBean insert(QBean qBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QBean update(QBean qBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		return 0;
+	}
+}
--- WebTravel/src/model/service/GameService.java
+++ WebTravel/src/model/service/GameService.java
@@ -1,16 +1,23 @@
 package model.service;
 
+import org.hibernate.Session;
+
 import model.bean.QBean;
 import model.dao.QDAO;
 import model.dao.jndi.QDAOjndi;
+import model.hibernate.HibernateUtil;
+import model.hibernate.QDAOHibernate;
 
 public class GameService {
-	
+	private QDAO qDao;
+	public GameService() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		qDao = new QDAOHibernate(session);
+	}	
 	public QBean getQuestion(){
-		QDAO q = new QDAOjndi();
 		QBean bean =new QBean();
-		int index = (int)(Math.random()*q.getCount()+1);
-		bean = q.select(index);
+	//	int index = (int)(Math.random()*q.getCount()+1);
+		bean = qDao.select(1);
 		return bean;
 	}
 
