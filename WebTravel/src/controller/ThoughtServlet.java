eabe9827764ac7766d196fb34f1adc90bd4fd8c2
Wayne <hs60801@yahoo.com.tw>
2016-01-21 10:11:19 +0800

    把SYS註解

--- WebTravel/src/controller/ThoughtServlet.java
+++ WebTravel/src/controller/ThoughtServlet.java
@@ -96,7 +96,7 @@ public class ThoughtServlet extends HttpServlet {
 		bean.setMemberId(temp5);
 		ThoughtBean inbean = ts.insert(bean);
 //		ThoughtBean b=thoughtDao.select(inbean.getThoughtId());
-		System.out.println("insert"+inbean);
+//		System.out.println("insert"+inbean);
 //		System.out.println("select"+b);
 		
 		//model執行結果，View
@@ -106,7 +106,7 @@ public class ThoughtServlet extends HttpServlet {
 			response.sendRedirect(path+"/thought/ThoughtPage.jsp");
 //			response.sendRedirect("index.jsp");
 //			request.getRequestDispatcher("game.jsp").forward(request, response);
-			System.out.println("AAABBB");
+//			System.out.println("AAABBB");
 		}else{
 			request.getRequestDispatcher("/thought/Thought.jsp").forward(request, response);
 		}
